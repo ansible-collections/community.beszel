977cdc843f37054ff8f7614be99975fb0b590fcb
dbrennand <52419383+dbrennand@users.noreply.github.com>
2025-08-30 14:14:04 +0100

    feat: `system` module

---
+++ plugins/modules/system.py
@@ -0,0 +1,306 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2025, Daniel Brennand <contact@danielbrennand.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+from ansible_collections.community.beszel.plugins.module_utils.pocketbase_utils import (
+    PocketBaseClient,
+)
+from pocketbase.errors import ClientResponseError
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: system
+
+short_description: Manage Beszel systems.
+
+version_added: "0.3.0"
+
+description: Create, update and delete Beszel systems.
+
+options:
+    url:
+        description: URL of the Beszel hub.
+        required: true
+        type: str
+    username:
+        description: Username used to authenticate to Beszel hub.
+        required: true
+        type: str
+    password:
+        description: Password used to authenticate to Beszel hub.
+        required: true
+        type: str
+    timeout:
+        description: Number of seconds to wait for the Beszel hub to respond.
+        required: false
+        type: float
+        default: 120
+    name:
+        description: Name of the Beszel system.
+        required: true
+        type: str
+    host:
+        description: IP address, FQDN or hostname of the Beszel system.
+        required: false
+        type: str
+    port:
+        description: Port of the Beszel system.
+        required: false
+        default: 45876
+        type: int
+    users:
+        description: List of users to add to the Beszel system.
+        required: false
+        type: list
+        elements: str
+    state:
+        description: State of the Beszel system.
+        required: false
+        default: present
+        type: str
+author:
+    - Daniel Brennand (@dbrennand)
+"""
+
+EXAMPLES = r"""
+- name: Register a Beszel system
+  community.beszel.system:
+    url: https://beszel.example.tld
+    username: admin@example.com
+    password: admin
+    name: instance
+    host: instance
+    port: 45876
+    state: present
+
+- name: Modify an existing Beszel system
+  community.beszel.system:
+    url: https://beszel.example.tld
+    username: admin@example.com
+    password: admin
+    name: instance
+    host: instance
+    port: 45877
+    state: present
+
+- name: Unregister a Beszel system
+  community.beszel.system:
+    url: https://beszel.example.tld
+    username: admin@example.com
+    password: admin
+    name: instance
+    state: absent
+"""
+
+RETURN = r"""
+changed:
+    description: Whether the system was changed.
+    type: bool
+    returned: always
+    sample: true
+system:
+    description:
+        - Information about the Beszel system.
+        - When state is absent and the system does not exist, the system will be returned as an empty dictionary.
+    type: dict
+    returned: always
+    sample: {
+            "collection_id": "2hz5ncl8tizk5nx",
+            "collection_name": "systems",
+            "created": "2025-08-30T07:48:04",
+            "expand": {},
+            "host": "instance",
+            "id": "q5y5h742bwueyns",
+            "info": {
+                "b": 0,
+                "bb": 40,
+                "c": 10,
+                "cpu": 0.06,
+                "dp": 4.09,
+                "h": "90ba6044d626",
+                "k": "6.15.11-orbstack-00539-g9885ebd8e3f4",
+                "la": [
+                    0,
+                    0,
+                    0
+                ],
+                "m": "",
+                "mp": 2.07,
+                "os": 0,
+                "t": 10,
+                "u": 29081,
+                "v": "0.12.6"
+            },
+            "name": "instance",
+            "port": "45876",
+            "status": "up",
+            "updated": "2025-08-30T11:08:36",
+            "users": [
+                "zsk3bb1p2uisg4g"
+            ]
+        }
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def run_module():
+    def get_existing_system(
+        module: AnsibleModule, client: PocketBaseClient, name: str
+    ) -> dict | None:
+        """Get the existing system given the name.
+
+        Args:
+            module (AnsibleModule): The Ansible module instance.
+            client (PocketBaseClient): The PocketBaseClient instance.
+            name (str): The name of the system to get.
+
+        Returns:
+            dict | None: The existing system if it exists, otherwise None.
+        """
+        try:
+            return client.collection("systems").get_first_list_item(
+                filter=f"name='{name}'"
+            ).__dict__
+        except ClientResponseError as e:
+            return None
+        except Exception as e:
+            module.fail_json(
+                msg=f"Failed to get existing system with name '{name}': {e}"
+            )
+
+    module_args = dict(
+        url=dict(type="str", required=True),
+        username=dict(type="str", required=True),
+        password=dict(type="str", required=True, no_log=True),
+        timeout=dict(type="float", required=False, default=120),
+        name=dict(type="str", required=True),
+        host=dict(type="str", required=False),
+        port=dict(type="int", required=False, default=45876),
+        users=dict(type="list", required=False, elements="str"),
+        state=dict(
+            type="str", required=False, default="present", choices=["present", "absent"]
+        ),
+    )
+
+    result = dict(changed=False, system={})
+
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+
+    try:
+        client = PocketBaseClient(
+            url=module.params["url"],
+            username=module.params["username"],
+            password=module.params["password"],
+            timeout=module.params["timeout"],
+        ).authenticate()
+    except Exception as e:
+        module.fail_json(msg=str(e))
+
+    if module.params["state"] == "present":
+        if module.params["host"] is None:
+            module.fail_json(msg="Host is required when state is present.")
+
+        # Attempt to get the existing system (if it exists)
+        existing_system = get_existing_system(module, client, module.params["name"])
+
+        # If we have a list of users, then we need to get the IDs of the users
+        if module.params["users"] is not None:
+            user_ids = []
+            for user in module.params["users"]:
+                try:
+                    user_ids.append(
+                        client.collection("users")
+                        .get_first_list_item(filter=f"email='{user}'")
+                        .id
+                    )
+                except Exception as e:
+                    module.fail_json(msg=f"Failed to get ID of user '{user}': {e}")
+        else:
+            # Get the ID of the current user
+            try:
+                user_ids = [
+                    client.collection("users")
+                    .get_first_list_item(filter=f"email='{module.params['username']}'")
+                    .id
+                ]
+            except Exception as e:
+                module.fail_json(
+                    msg=f"Failed to get ID of current user '{module.params['username']}': {e}"
+                )
+
+        # If we have an existing system, then we need to determine if the
+        # new config is different from the existing config
+        if existing_system is not None:
+            if (
+                existing_system["host"] != module.params["host"]
+                or int(existing_system["port"]) != module.params["port"]
+                or existing_system["users"] != user_ids
+            ):
+                # We need to update the system
+                try:
+                    data = client.collection("systems").update(
+                        id=existing_system["id"],
+                        body_params={
+                            "host": module.params["host"],
+                            "port": module.params["port"],
+                            "users": user_ids,
+                        },
+                    )
+                    result["system"] = data.__dict__
+                    result["changed"] = True
+                except Exception as e:
+                    module.fail_json(
+                        msg=f"Failed to update system '{module.params['name']}': {e}"
+                    )
+            else:
+                # The system is already in the desired state
+                result["system"] = existing_system
+                result["changed"] = False
+        else:
+            # We need to create a new system
+            try:
+                data = client.collection("systems").create(
+                    body_params={
+                        "name": module.params["name"],
+                        "host": module.params["host"],
+                        "port": module.params["port"],
+                        "users": user_ids,
+                    }
+                )
+                result["system"] = data.__dict__
+                result["changed"] = True
+            except Exception as e:
+                module.fail_json(
+                    msg=f"Failed to create system '{module.params['name']}': {e}"
+                )
+
+    elif module.params["state"] == "absent":
+        existing_system = get_existing_system(module, client, module.params["name"])
+        if existing_system is None:
+            # The system does not exist, so we don't need to do anything
+            result["changed"] = False
+        else:
+            # We need to delete the system
+            try:
+                client.collection("systems").delete(id=existing_system["id"])
+                result["changed"] = True
+                result["system"] = existing_system
+            except Exception as e:
+                module.fail_json(
+                    msg=f"Failed to delete system '{module.params['name']}': {e}"
+                )
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == "__main__":
+    main()
